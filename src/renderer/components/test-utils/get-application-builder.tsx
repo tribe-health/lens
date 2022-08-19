a8458d68e322766f410fd0d3bdea8077b02ed653
Sebastian Malton <sebastian@malton.name>
2022-08-19 11:28:00 -0400

    Remove injectable for time since we should just use useMockTime
    Signed-off-by: Sebastian Malton <sebastian@malton.name>

--- src/renderer/components/test-utils/get-application-builder.tsx
+++ src/renderer/components/test-utils/get-application-builder.tsx
@@ -57,7 +57,6 @@ import activeKubernetesClusterInjectable from "../../cluster-frame-context/activ
 import { catalogEntityFromCluster } from "../../../main/cluster-manager";
 import namespaceStoreInjectable from "../+namespaces/store.injectable";
 import { isAllowedResource } from "../../../common/cluster/is-allowed-resource";
-import getMillisecondsFromUnixEpochInjectable from "../../../common/utils/date/get-milliseconds-from-unix-epoch.injectable";
 import createApplicationWindowInjectable from "../../../main/start-main-application/lens-window/application-window/create-application-window.injectable";
 import type { CreateElectronWindow } from "../../../main/start-main-application/lens-window/application-window/create-electron-window.injectable";
 import createElectronWindowInjectable from "../../../main/start-main-application/lens-window/application-window/create-electron-window.injectable";
@@ -112,12 +111,6 @@ export interface ApplicationBuilder {
 
   applicationMenu: {
     click: (path: string) => void;
-  };
-  time: {
-    advanceBy: {
-      seconds: (value: number) => void;
-      milliseconds: (value: number) => void;
-    };
   };
   preferences: {
     close: () => void;
@@ -191,7 +184,6 @@ export const getApplicationBuilder = () => {
     overrideChannelsForWindow(windowDi, windowId);
 
     windowDi.register(rendererExtensionsStateInjectable);
-    windowDi.override(getMillisecondsFromUnixEpochInjectable, () => () => currentTimeMs);
 
     windowDi.override(
       currentlyInClusterFrameInjectable,
@@ -247,22 +239,9 @@ export const getApplicationBuilder = () => {
   mainDi.override(createElectronWindowInjectable, () => createElectronWindowFake);
 
   let applicationHasStarted = false;
-  let currentTimeMs = Date.parse("2000-01-01 12:00:00am");
-
-  mainDi.override(getMillisecondsFromUnixEpochInjectable, () => () => currentTimeMs);
 
   const builder: ApplicationBuilder = {
     mainDi,
-    time: {
-      advanceBy: {
-        milliseconds: (value) => {
-          currentTimeMs += value;
-        },
-        seconds: (value) => {
-          currentTimeMs += (value * 1000);
-        },
-      },
-    },
     applicationWindow: {
       closeAll: () => {
         const closeAll = mainDi.inject(closeAllWindowsInjectable);
